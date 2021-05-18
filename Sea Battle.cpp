0ab0d0c01a5b807b5625f93269f3eb6be0d36ac9
Olek_rg45@student.itstep.org <Olek_rg45@student.itstep.org>
2021-05-18 22:54:40 +0300

    Create a constants

--- Sea Battle.cpp
+++ Sea Battle.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
+const int HEIGHT = 10;
+const int WIDTH = 10;
+const int SHIPS = 5;
+const int SHIPS_LENGTH = 3;
+
+
+
+
+
+
+
+
+
 int main()
 {
     cout << "Hello" << endl;
